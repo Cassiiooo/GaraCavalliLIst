9e2fbe3b0d496222e4febd2948be43f8f9da019a
Michele831Poli <michelepolidorii@icloud.com>
2025-11-07 09:15:41 +0100

    azzopix

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -3,24 +3,52 @@ import com.sun.tools.javac.Main;
 public class Cavallo extends Thread {
     private final String name;
     private int lentezza;
-    public Cavallo (String name, int lentezza){
+    private boolean azzoppato = false;
+
+    public Cavallo(String name, int lentezza) {
         super();
         this.name = name;
         this.lentezza = lentezza;
     }
 
     @Override
-    public void run(){
-        System.out.println("Cavallo " + name + "comincia il suo galoppo");
-        for (int i = 1; i <= 10; i++ ){
+    public void run() {
+        System.out.println("Cavallo " + name + " comincia il suo galoppo.");
+
+        for (int i = 1; i <= 10; i++) {
+            if (azzoppato) {
+                System.out.println("Cavallo " + name + " è stato azzoppato e si ritira dalla gara.");
+                return;
+            }
             try {
                 sleep(lentezza);
             } catch (InterruptedException a) {
-                throw new RuntimeException(a);
+                System.out.println("Cavallo " + name + " è stato fermato.");
+                return;
             }
-            System.out.println(name + " cavalca - passo: " + 1);
+
+            System.out.println(name + " cavalca - passo: " + i);
+
+            if (i == 10 && Main.getPrimo().equals("")) {
+                Main.setPrimo(name);
+            }
         }
     }
-    protected int getLentezza(){return lentezza;}
-    protected void setLentezza(){ this.lentezza = lentezza;}
+
+    public void azzoppa() {
+        this.azzoppato = true;
+        this.interrupt();
+    }
+
+    protected int getLentezza() {
+        return lentezza;
+    }
+
+    protected void setLentezza(int lentezza) {
+        this.lentezza = lentezza;
+    }
+
+    public String getNome() {
+        return name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    static String primo="";
+    static String primo = "";
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String tmpS;
         int tmp;
-        ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>();
+        ArrayList<Cavallo> listaCavallo = new ArrayList<>();
+
         for (int i = 1; i <= 4; i++) {
-            System.out.println("Inserisci il nome del cavallo " + i);
-            tmpS =  input.nextLine();
-            System.out.println("Inserisci la lentezza del cavallo " + i);
+            System.out.println("Inserisci il nome del cavallo " + i + ": ");
+            tmpS = input.nextLine();
+            System.out.println("Inserisci la lentezza del cavallo " + i + ": ");
             tmp = input.nextInt();
-            String v = input.nextLine(); //prende il \n
-            Cavallo c=new Cavallo(tmpS, tmp);
+            input.nextLine();
+            Cavallo c = new Cavallo(tmpS, tmp);
             listaCavallo.add(c);
         }
-        for(Cavallo c: listaCavallo){
+
+        for (Cavallo c : listaCavallo) {
             c.start();
         }
-        for(Cavallo c: listaCavallo){
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Random rand = new Random();
+        Cavallo sfortunato = listaCavallo.get(rand.nextInt(listaCavallo.size()));
+        System.out.println("Il cavallo " + sfortunato.getNome() + " è stato azzoppato!");
+        sfortunato.azzoppa();
+
+        for (Cavallo c : listaCavallo) {
             try {
                 c.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        System.out.println("Il primo cavallo: " + primo);    }
+
+        if (primo.equals("")) {
+            System.out.println("Nessun cavallo è arrivato al traguardo.");
+        } else {
+            System.out.println("Il primo cavallo arrivato è: " + primo);
+        }
+    }
 
     public static String getPrimo() {
         return primo;
     }
+
     public static void setPrimo(String primo) {
         Main.primo = primo;
     }
